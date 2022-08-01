eb32330b1d4f24f20a88eb21ef3ae2960694e868
jykim11 <wnsdud19@gmail.com>
2022-08-01 19:12:08 -0400

    Made changes to the files. Added dependencies

---
+++ app.js
@@ -0,0 +1,14 @@
+$(function () {
+  $("#datepicker").datepicker();
+});
+
+const appointmentPicker = new AppointmentPicker(
+  document.getElementById("time-form"),
+  {
+    interval: 30,
+    mode: "12h",
+    startTime: 09,
+    endTime: 17,
+    disabled: ["12:00", "12:30"],
+  }
+);
